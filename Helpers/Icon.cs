59267fefaad56e73a241ce7067bac5a1444f4904
Till Knollmann <till-knollmann@outlook.de>
2025-10-29 00:25:00 +0100

    🧑‍💻 made icon fields readonly

--- Helpers/Icon.cs
+++ Helpers/Icon.cs
@@ -2,15 +2,15 @@
 {
     internal class Icon
     {
-        internal static string CLOCK = "clock.png";
-        internal static string CODE = "code.png";
-        internal static string COPY = "copy.png";
-        internal static string EXTERNAL_LINK = "external.png";
-        internal static string NEW_SNIPPET = "new.png";
-        internal static string SEARCH = "search.png";
-        internal static string DISCONNECT = "disconnected.png";
-        internal static string SNIPPET = "snippet.png";
-        internal static string DELETE = "delete.png";
-        internal static string EDIT = "edit.png";
+        internal static readonly string CLOCK = "clock.png";
+        internal static readonly string CODE = "code.png";
+        internal static readonly string COPY = "copy.png";
+        internal static readonly string EXTERNAL_LINK = "external.png";
+        internal static readonly string NEW_SNIPPET = "new.png";
+        internal static readonly string SEARCH = "search.png";
+        internal static readonly string DISCONNECT = "disconnected.png";
+        internal static readonly string SNIPPET = "snippet.png";
+        internal static readonly string DELETE = "delete.png";
+        internal static readonly string EDIT = "edit.png";
     }
 }
